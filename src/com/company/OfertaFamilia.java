8831d82e21490549eafcfc6833d61fd6d8968f8f
sipwarriper <sipwarriper@gmail.com>
2018-01-22 20:02:48 +0100

    oferta amb gestio de dates

--- src/com/company/OfertaFamilia.java
+++ src/com/company/OfertaFamilia.java
@@ -1,18 +1,26 @@
 package com.company;
 
+import java.util.Date;
+
 public class OfertaFamilia extends Oferta{
     private AbstractProduct prod;
     private Familia familia;
     private int descompte;
+    private Date dataInici,dataFi;
 
-    public OfertaFamilia(AbstractProduct ap, Familia fam, int desc){
+
+    public OfertaFamilia(AbstractProduct ap, Familia fam, int desc, Date inici, Date fi){
         prod=ap;
         familia=fam;
         descompte=desc;
+        dataFi=fi;
+        dataInici=inici;
     }
 
     @Override
     public int ObtenirPreu() {
         return descompte*prod.ObtenirPreu();
     }
+
+    public boolean esVigent(Date avui){ return (avui.before(dataFi) && avui.after(dataInici));  }
 }
