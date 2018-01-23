92870643c479f05874f1febe818c1e6dd8fc9e40
sipwarriper <sipwarriper@gmail.com>
2018-01-23 09:28:49 +0100

    tota la avançada dahir

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,16 @@
 package com.company;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    private Date avui;
+    private ArrayList<Producte> productes;
+    private ArrayList<Familia> families;
+    private Botiga botiga;
+    private Magatzem magatzem;
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
@@ -32,7 +41,46 @@ public class Main {
         }
         reader.close();
     }
+    public static void creacioFamilies(){
 
+    }
+
+    public static void creacioProductes() {
+        String Linia;
+        Producte temp;
+
+        BufferedReader sProductes = null;
+        try {
+            sProductes = new BufferedReader(new FileReader("productes.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (sProductes != null){
+            try{
+                Linia = sProductes.readLine();
+                while (Linia!=null){
+                    String [] data = Linia.split(" ");
+                    int codiProducte = Integer.parseInt(data[0]);
+                    int codiFamilia = Integer.parseInt(data[1]);
+                    String nomProducte = data[2].replaceAll("_", " ");
+                    int EstocBotiga = Integer.parseInt(data[3]);
+                    int EstocMagatzem = Integer.parseInt(data[4]);
+                    double preu = (Integer.parseInt(data[4]))/100.0;
+                    temp = new Producte(codiProducte,nomProducte, preu);
+                }
+
+            } catch (java.io.IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void cracioOfertes(){
+
+    }
+    public static void inicialitzarPrograma(){
+
+    }
     public static void PassarPerCaixa(){
         Carreto carr=new Carreto();
         
